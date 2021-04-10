9d253a798bea933712f10bf0d51b9a3d8a0f70fe
yichen96 <liuyichenpanda@gmail.com>
2021-04-10 01:08:44 +0100

    Initial Commit

---
+++ Choredo/ChoredoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoredoApp.swift
+//  Choredo
+//
+//  Created by Yichen Liu on 10/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChoredoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
