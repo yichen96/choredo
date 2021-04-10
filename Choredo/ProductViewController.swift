01510f878e895683978212408a60c9aff5c2d318
yichen96 <liuyichenpanda@gmail.com>
2021-04-10 18:10:11 +0100

    added basic image classification model

---
+++ Choredo/ProductViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+import AVFoundation
+import Vision
+
+class ProductViewController: UIViewController {
+    
+    @IBOutlet var productView: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var productPhoto: UIImageView!
+    
+    var productID: String!
+    var productCatalog: [String: [String: Any]]!
+    
+    @IBAction func dismissProductView(_ sender: Any) {
+        dismiss(animated: true) {
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Read the product catalog from the plist file into the dictionary.
+        if let path = Bundle.main.path(forResource: "ProductCatalog", ofType: "plist") {
+            productCatalog = NSDictionary(contentsOfFile: path) as? [String: [String: Any]]
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Give the view rounded corners.
+        productView.layer.cornerRadius = 10
+        productView.layer.masksToBounds = true
+        
+        if productID != nil {
+            guard productCatalog[productID] != nil else {
+                return
+            }
+            label.text = productCatalog[productID]?["label"] as? String
+            if let productImage = UIImage(named: productID + ".png") {
+                productPhoto.image = productImage
+            }
+        }
+    }
+}
--- ChoredoTests/AppDelegateTests.swift
+++ ChoredoTests/AppDelegateTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import Choredo
+@testable import AppDelegate
 
-class ChoredoTests: XCTestCase {
+class AppDelegateTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
